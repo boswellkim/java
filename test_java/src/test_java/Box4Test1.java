bd753f5e52f92ff4486de251917179dc2ad0d963
boswellkim <enlil5454@gmail.com>
2015-11-16 11:41:19 +0900

    2015.11.16.Cons

---
+++ test_java/src/test_java/Box4Test1.java
@@ -0,0 +1,26 @@
+package test_java;
+
+class Box4 {
+	int width;
+	int height;
+	int depth;
+
+	public Box4(int w, int h, int d) {
+		width = w;
+		height = h;
+		depth = d;
+	}
+}
+
+public class Box4Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1505083-Kyungmin Kim");
+		Box4 mybox1 = new Box4(10, 20, 30);
+		int vol = mybox1.width * mybox1.height * mybox1.depth;
+		System.out.println("result : " + vol);
+
+	}
+
+}
