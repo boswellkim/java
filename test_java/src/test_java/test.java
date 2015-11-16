847cdd93fb39abe8c6a84619a47e39b331414473
boswellkim <enlil5454@gmail.com>
2015-11-16 10:12:40 +0900

    InitialTest1

--- test_java/src/test_java/test.java
+++ test_java/src/test_java/test.java
@@ -1,10 +1,28 @@
 package test_java;
-
+class Initial{
+	int number;
+	double rate;
+	String name;
+	int[] score;
+	public void aMethod(){
+		int count;
+		System.out.println(number);
+		//System.out.println(count);
+	}
+}
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1505083-±è°æ¹Î");
+		int var1;
+		double var2;
+		Initial ob1 = new Initial();
+		//System.out.println("var1 : "+var1);
+		System.out.println("number : "+ob1.number);
+		System.out.println("rate : "+ob1.rate);
+		System.out.println("name : "+ob1.name);
+		System.out.println("score : "+ob1.score);
 
 	}
 
